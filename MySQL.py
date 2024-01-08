3dabf6e877ad48d62642dd6aca12ab3d90b67a0e
LucianoAPZ <lucianoanpoza@gmail.com>
2024-01-08 21:06:22 +0100

    En datos ya tienen cada mapa lasposicion inicial que empieza el jugador y hemos solucionado el problema del nombre en plot, y el MySQL.py ya estaria bien ya que a erik le funciona pero hay que instalar el connector-python y el python3-pip para que funcione

--- MySQL.py
+++ MySQL.py
@@ -1,23 +1,7 @@
 import mysql.connector
 
-try:
-    conexion = mysql.connector.connect(
-        user="root",
-        host="127.0.0.1",
-        database="ZeldaBBDD",
-        port="3306",
-    )
-
-    micursor = conexion.cursor()
-
-    micursor.execute("SHOW DATABASES")
-
-    consulta = micursor.fetchall()
-
-    for db in consulta:
-        print(db)  
-
-    conexion.close() 
-
-except mysql.connector.Error as error:
-    print("Error al conectar a la base de datos:", error)
+mybd = mysql.connector.connect(
+    user="root",
+    host="127.0.0.1",
+    database="ZeldaBBDD",
+)
