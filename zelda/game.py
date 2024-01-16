a78b06f5d3ad06eb82fb0318760babb52c648095
ErikRojaas <152431347+ErikRojaas@users.noreply.github.com>
2024-01-16 18:58:06 +0100

    actualizacion (guardado por si acaso)

--- zelda/game.py
+++ zelda/game.py
@@ -13,159 +13,167 @@ enemic = False
 # Habrá que cambiar esto por que se activen los objetos que tiene al lado
 
 def game(): # Hay que mirar como se pondria para cuando eliges una partida guardada.
-    exit = False
-    while exit == False:
-        exit = f.menu_principal()
-        if exit == False:
+    exit = False 
+    while exit == False: # Bucle para que cuando le des a exit en el menu salga del juego
+        exit = f.menu_principal() # Ejecutamos el menu principal, devuelve un booleano, si es falso es porque has pulsado exit
+        if exit == False: # Salir de la función
             exit = True
             return True
         
-        posicionplayer = d.dades["hyrule"]["position"]
-        mapaActual = d.localitzacions["hyrule"]
-        f.zorro_visivilidad() 
-        final = False
-        while final == False:
+        posicionplayer = d.dades["hyrule"]["position"] # guardamos posicion inicial
+        mapaActual = d.localitzacions["hyrule"] # guardamos mapa inicial
+        f.zorro_visivilidad() # miramos visibilidad del zorro
+        final = False 
+        while final == False: # Bucle para la partida
             f.limpiar_pantalla()
-            
-            if d.ganon["vida"] == 0: # Crear diccionario de ganon con sus vidas
+            # Crear diccionario de ganon con sus vidas
+            if d.ganon["vida"] == 0: # si la vida de ganon es 0, se muestra la pantalla de win
                         f.limpiar_pantalla()
                         f.imprimirmapa_menu(d.diccionarioMenuPrincipal["zelda_saved"])
-                        d.texto_prompt.append("It has been an exhausting fight, but with persistence, you have achieved it.")
+                        d.texto_prompt.append("It has been an exhausting fight, but with persistence, you have achieved it.") # Se añade esta frase al prompt
                         f.prompt()
                         opc = input("What to do now? ")
-                        while opc.lower() != "continue":
+                        while opc.lower() != "continue": # Mientras no se escriba la opcion continue, no saldran de la pantalla de win
                             f.limpiar_pantalla()
                             f.imprimirmapa_menu(d.diccionarioMenuPrincipal["zelda_saved"])
                             d.texto_prompt.append("Invalid action")
                             f.prompt()
                             opc = input("What to do now? ")
                         
-                        d.texto_prompt = []
-                        d.ganon["vida"] = 9
-                        return False
+                        # Esto hay que camiarlo, y resetear todo
+                        d.texto_prompt = [] # Se reinicia el prompt
+                        d.ganon["vida"] = 9 # Se reinician las vidas de Ganon
+                        return False # Devolvemos falso para no terminar el bulce y que vuelva a aparecer el menu principal
             
-            elif d.jugador["vidas"] == 0:
+            elif d.jugador["vidas"] == 0: # si la vida del jugador es 0, se muestra la pantalla de muerte
                 f.imprimirmapa_menu(d.diccionarioMenuPrincipal["link_death"])
-                d.texto_prompt.append("Nice try, you died, game is over")
+                d.texto_prompt.append("Nice try, you died, game is over") # Se añade al prompt esta frase
                 f.prompt()
-                opc = input("What to do now? ")
+                opc = input("What to do now? ") 
             
-                while opc.lower() != "continue":
+                while opc.lower() != "continue": # Mientras no escriban la opcion continue, no saldra de esta pantalla
                     f.limpiar_pantalla()
                     f.imprimirmapa_menu(d.diccionarioMenuPrincipal["link_death"])
                     d.texto_prompt.append("Invalid action")
                     f.prompt()
                     opc = input("What to do now? ")
                 
-                d.texto_prompt = []
-                d.jugador["vidas"] = 3
-                return False
+                # Esto hay que cambiarlo, y reiniciar todo (esto es para pruebas)
+                d.texto_prompt = [] # Se reinicia el prompt
+                d.jugador["vidas"] = 3 # Se reinician las vidas
+                return False # Devolvemos falso para no terminar el bulce y que vuelva a aparecer el menu principal
             
-            mapaActual = f.obtenerMapa(mapaActual, posicionplayer)
-            f.menuInferior(mapaActual)
-            f.prompt()
-            select = input("What to do now? ")
-            d.texto_prompt.append(select)
+            mapaActual = f.obtenerMapa(mapaActual, posicionplayer) # Se imprime el mapa, y se deja al jugador en su posicion
+            f.menuInferior(mapaActual) # se imprime el menu inferior
+            f.prompt() # se imprime el prompt
+            select = input("What to do now? ") # select de la accion a realizar
+            d.texto_prompt.append(select) # se añade el la accion al prompt
             
-            if mapaActual == d.localitzacions["hyrule"] or mapaActual == d.localitzacions["gerudo"] or mapaActual == d.localitzacions["death"] or mapaActual == d.localitzacions["necluda"]:
+            if mapaActual == d.localitzacions["hyrule"] or mapaActual == d.localitzacions["gerudo"] or mapaActual == d.localitzacions["death"] or mapaActual == d.localitzacions["necluda"]: # si no estas en castillo, movimiento y acciones normales
             
-                if select[0:7].lower() == "go left" or select[0:8].lower() == "go right" or select[0:5].lower() == "go up" or select[0:7].lower() == "go down":
-                    retorno = f.moverPersonaje(mapaActual, select, posicionplayer) 
-                    posicionplayer = [retorno[1], retorno[2]]
+                if select[0:7].lower() == "go left" or select[0:8].lower() == "go right" or select[0:5].lower() == "go up" or select[0:7].lower() == "go down": # mover personaje
+                    retorno = f.moverPersonaje(mapaActual, select, posicionplayer) # llanmada a la funcion de mover el personaje
+                    posicionplayer = [retorno[1], retorno[2]] # nueva posicion del jugador
                 
-                elif select[0:14].lower() == "show inventory":
-                    if select.lower() == "show inventory food":
+                elif select[0:14].lower() == "show inventory": # si se escribe show inventory
+                    if select.lower() == "show inventory food": # se cambia al inventario de comida
                         d.select = "show inventory food"
                     
-                    elif select.lower() == "show inventory weapons":
+                    elif select.lower() == "show inventory weapons": # se cambia al inventario de armas
                         d.select = "show inventory weapons"
                     
-                    elif select.lower() == "show inventory main":
+                    elif select.lower() == "show inventory main": # se cambia al inventario principal
                         d.select = "show inventory main"
                     
-                    else:
+                    else: # opcion incorrecta se añade al prompt
                         d.texto_prompt.append("Invalid action")
                 
-                elif select[0:8].lower() == "show map":
+                elif select[0:8].lower() == "show map": # Muestra el mapa de la region
                     f.mostrar_mapa()
                 
-                elif select[0:3].lower() == "eat":
+                elif select[0:3].lower() == "eat": # Consumir comida
                     # Falta la funcion de comer 
                     print("a")
                 
-                elif select[0:11].lower() == "unequip the": # Hay que mirar por que cuando desequipo un arma sigue saliendo en el inventario.
+                elif select[0:11].lower() == "unequip the": # Desequipar arma
+                    # Hay que mirar por que cuando desequipo un arma sigue saliendo en el inventario.
                     d.texto_prompt.append(f.desequiparArma(select))
                     
-                elif select[0:9].lower() == "equip the": # Me da error la funcion
+                elif select[0:9].lower() == "equip the": # Equipar arma
+                    # Me da error la funcion
                     f.equiparArma(select)
                 
-                elif select[0:4].lower() == "cook" and cook == True: 
+                elif select[0:4].lower() == "cook" and cook == True:  # Cocinar comida, cuando estas al lado de una C
                     f.cocinar(select, d.inventarioComida)
                     
-                elif select.lower() == "attack" and enemic == True:
+                elif select.lower() == "attack" and enemic == True: # Atacar a un enemigo, cuando esta a tu lado
                     # Falta funcion de enemigo
                     print("a")
                 
-                elif select.lower() == "attack" and d.visibilidad_zorro == True:
+                elif select.lower() == "attack" and d.visibilidad_zorro == True: # Atacar a un zorro, cuando estas al lado de una F
                     f.zorro() # Falta quitarle vida al arma y añadir carne al inventario
                 
-                elif select.lower() == "attack" and tree == True:
+                elif select.lower() == "attack" and tree == True: # Atacar a un arbol, cuando estas al lado de una T
                     f.arbol(d.jugador["arma_actual"]) # Falta acabar la funcion de arboles, da error con la vida
                     
-                elif select.lower() == "attack" and cesped == True: # Lo mismo que cocinar
+                elif select.lower() == "attack" and cesped == True: # Atacat al cesped, cuando estas en un " "
                     f.cesped()
                 
-                elif select.lower() == "fish" and fish == True:
+                elif select.lower() == "fish" and fish == True: # Pescar cuando estas al lado de una ~
                     f.agua() # Falta añadir al inventario el pez conseguido
                 
-                elif select.lower() == "open sanctuary" and santuario == True:
+                elif select.lower() == "open sanctuary" and santuario == True: # Abrir un santuario, cuando estas al lado de un santuario 
                     f.abrir_santuario() # Hay que crear un diccionario o lista con los santuarios y que cuando abres uno se ponga en True en el diccionario.
                     # Tambien hay que mirar que al mostrar el mapa de la región los santuarios abiertos se tiene que eliminar el ?, y igual con los del mapa jugable
                 
-                elif select[0:5].lower() == "go to": # en el pdf no pone nada, asi que a castle se puede ir desde cualquier sitio, y dentro de castle no se puede ir a otro sitio, tienes que atacar a ganon o escribir back y volver a la ultima region donde has estado.
+                elif select[0:5].lower() == "go to": # Cambiar de región
+                    # en el pdf no pone nada, asi que a castle se puede ir desde cualquier sitio, y dentro de castle no se puede ir a otro sitio, tienes que atacar a ganon o escribir back y volver a la ultima region donde has estado.
                     retorno = f.cambiar_mapa(select, mapaActual)
                     mapaActual = retorno[0]
                     posicionplayer = retorno[1]
                     
                 
-                elif select[0:5].lower() == "go by":
+                elif select[0:5].lower() == "go by": # moverte al objeto indicado más cercano a donde estas
                     # Falta terminar la funcion para del movimiento cercano
                     print("a")
                 
-                elif select.lower() == "open chest" and chest == True:
+                elif select.lower() == "open chest" and chest == True: # Abrir cofre, cuando estas al lado de una M o W
                     f.cofre() # Falta ver como hacemos lo de los cofres abiertos, que se reinicien y tal.
                     
-                elif select[0:5].lower() == "cheat": # Falta terminar la funcion de trucos
+                elif select[0:5].lower() == "cheat": # Trucos
+                    # Falta terminar la funcion de trucos
                     f.trucos(select)
                 
-                else:
+                else: # Opcion invalida, se añade al prompt
                     d.texto_prompt.append("Invalid action")
             
-            else:
-                if select[0:7].lower() == "go left" or select[0:8].lower() == "go right":
+            else: # Si estas en el mapa del castillo, tienes movimientos limitados
+                if select[0:7].lower() == "go left" or select[0:8].lower() == "go right": # Solo puedes moverte a derecha o izquierda
                     retorno = f.moverPersonajeGanon(mapaActual, select, posicionplayer)
-                    posicionplayer = [retorno[1], retorno[2]]
+                    posicionplayer = [retorno[1], retorno[2]] # Cambiar posicion a nueva despues del movimiento
                 
-                elif select.lower() == "back": 
-                    d.texto_prompt.append("You are now in " + d.mapa_anterior) 
-                    mapaActual = d.localitzacions[d.mapa_anterior]
-                    posicionplayer = d.dades[d.mapa_anterior]["position"]
+                elif select.lower() == "back": # Volver a la ultima región, desde donde has viajado hasta el castillo
+                    d.texto_prompt.append("You are now in " + d.mapa_anterior) # Se añade al prompt
+                    mapaActual = d.localitzacions[d.mapa_anterior] # Se cambia el mapa
+                    posicionplayer = d.dades[d.mapa_anterior]["position"] # Se cambia la posicion
                 
-                elif select.lower() == "attack" and posicionplayer == [9,21]: # Falta funcion para atacar a ganon
+                elif select.lower() == "attack" and posicionplayer == [9,21]: # Atacar a Ganon, cuando estas a su lado
+                    # Falta funcion para atacar a ganon
                     aux = d.ganon["vida"]
                     d.ganon["vida"] = aux - 1 
                     f.frase_ganon()
                 
-                elif select[0:5].lower() == "cheat": # Falta funcion trucos
+                elif select[0:5].lower() == "cheat": # Trucos
+                    # Falta terminar funcion trucos
                     f.trucos(select)
                 
-                else:
+                else: # Opción invalida, se añade al prompt
                     d.texto_prompt.append("Invalid action")
                         
-                              
+juego = True                         
 
-while True:            
-    terminar = game()
+while juego == True: # Mientras no se eliga la opción exit del menu principal           
+    terminar = game() # Se ejecuta el juego
     
-    if terminar == True:
-        break
+    if terminar == True: # Si la opcion elegida ha sido exit en el menu principal
+        juego = False # Termina el bucle
