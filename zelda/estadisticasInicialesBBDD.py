6951a293f0272cac15a89760d45f4a140123c7cb
JorgeCortes02 <120786819+JorgeCortes02@users.noreply.github.com>
2024-01-19 21:55:29 +0100

    hggfh

--- zelda/estadisticasInicialesBBDD.py
+++ zelda/estadisticasInicialesBBDD.py
@@ -1,7 +1,38 @@
-jugadores = "Select distinct  user_name, date_started from game;"
+import mysql.connector
+
 
 partidasxJugador = "Select user_name, count(*) from game group by user_name;"
 
-ArmasUsuarios = "SELECT g.user_name AS Usuario, w.weapon_name AS Arma, COUNT(*) AS CantidadObtenida, MAX(g.date_started) AS FechaPartidaMasUsos FROM game g JOIN game_weapons w ON g.game_id = w.game_id GROUP BY g.user_name, w.weapon_name ORDER BY Usuario, CantidadObtenida DESC;"UPDATE game
-mysql>  SELECT     g.user_name AS Usuario,     gf.food_name AS Alimento,     SUM(gf.quanntity_remaining) AS CantidadObtenida FROM     game g JOIN     game_food
-gf ON g.game_id = gf.game_id GROUP BY     g.user_name, gf.food_name ORDER BY     Usuario, CantidadObtenida DESC;
+ArmasUsuarios = "SELECT g.user_name AS Usuario, w.weapon_name AS Arma, COUNT(*) AS CantidadObtenida, MAX(g.date_started) AS FechaPartidaMasUsos FROM game g JOIN game_weapons w ON g.game_id = w.game_id GROUP BY g.user_name, w.weapon_name ORDER BY Usuario, CantidadObtenida DESC;"
+
+
+# Conectar a la base de datos
+db = mysql.connector.connect(
+    host="172.187.226.29",  # Cambia a tu dirección IP
+    user="root2",
+    passwd="EsteveTerradas2023.", 
+    database="ZeldaBBDD"
+)
+
+
+
+def consultasBBDD():
+
+    jugadores = "Select distinct  user_name, date_started from game;"
+
+    cursor.execute(ArmasUsuarios)
+    resultados = cursor.fetchall()
+
+    for element in resultados:
+        print(element)
+
+
+     
+
+
+
+
+
+# Crear un cursor
+cursor = db.cursor()
+consultasBBDD()
